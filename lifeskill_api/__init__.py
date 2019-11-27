2285add192a995b8f09b4c91c22d62e61b2fcde6
Faris <Coding@omg.local>
2019-11-27 13:34:48 +0800

    connecting front to back 50% progress

--- lifeskill_api/__init__.py
+++ lifeskill_api/__init__.py
@@ -1,5 +1,6 @@
 from lifeskill_api.blueprints.students.views import students_api_blueprint
 from lifeskill_api.blueprints.calendar.views import calendar_api_blueprint
+from lifeskill_api.blueprints.audio.views import audio_api_blueprint
 from app import app
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
@@ -9,5 +10,5 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 app.register_blueprint(students_api_blueprint, url_prefix='/api/v1/students')
 app.register_blueprint(calendar_api_blueprint, url_prefix='/api/v1/calendar')
-
+app.register_blueprint(audio_api_blueprint, url_prefix='/api/v1/audio')
 jwt = JWTManager(app)
