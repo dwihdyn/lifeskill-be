621e6565ba5df003445bb4d2c2a4042eb1dd98bb
Faris <Coding@omg.local>
2019-11-28 18:37:15 +0800

    connection to front-end 100% working

--- lifeskill_api/blueprints/audio/views.py
+++ lifeskill_api/blueprints/audio/views.py
@@ -22,7 +22,7 @@ import api.IdentifyFile
 import api.IdentificationResponse
 import api.IdentificationProfile
 import tempfile
-
+import ffmpeg
 
 audio_api_blueprint = Blueprint(
     'audio_api', __name__, template_folder='templates')
@@ -55,3 +55,44 @@ def voice():
 #         assistant(my_command())
 #     else:
 #         sofia_response("I do not listen to you")
+<<<<<<< HEAD
+=======
+
+
+@audio_api_blueprint.route('/upload', methods=['POST'])
+def takeAudio():
+
+    print('success')
+    # res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000')
+    # breakpoint()
+    temp = tempfile.NamedTemporaryFile(suffix='.wav')
+    data = request.files['audio'].read()
+    temp.write(data)
+    temp.seek(0)
+    audio_file = temp.name
+    file_path = "/".join(audio_file.split("/")[0:-1])
+    file_name = audio_file.split("/")[-1]
+    subprocess.call(['ffmpeg', '-i', audio_file, '-ar',
+                     '16000', (file_path + '/my_' + file_name)])
+
+    # os.write(audio_file, audio)
+
+    # audio, filename = tempfile.mkstemp()
+
+    # breakpoint()
+    # os.write(audio, request.files['file'])
+    # audio_file = filename + '/' + audio.read()
+    # audio = audio.read()
+    speaker = api.IdentifyFile.identify_file(
+        sub_key, file_path + '/my_' + file_name, True, profile)
+    # This functions takes input from microphone and converts input into a string
+
+    if speaker == profile:
+        subprocess.call(['ffmpeg', '-i', audio_file, '-ar',
+                         '44000', (file_path + '/my2_' + file_name)])
+        assistant(my_command(file_path + '/my2_' + file_name))
+    else:
+        sofia_response("I do not listen to you")
+
+    return print('IM THE BEST')
+>>>>>>> connection to front-end 100% working
