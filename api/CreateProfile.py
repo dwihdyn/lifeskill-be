e29b608a7ae4b6ea12e6028acdf06ac838133f88
Faris <Coding@omg.local>
2019-12-02 19:58:19 +0800

    going to fix point giving and speaker recog

--- api/CreateProfile.py
+++ api/CreateProfile.py
@@ -1,7 +1,10 @@
+<<<<<<< HEAD
 import IdentificationServiceHttpClientHelper
 import sys
 
 
+=======
+>>>>>>> connecting front to back 50% progress
 def create_profile(subscription_key, locale):
     """Creates a profile on the server.
     Arguments:
--- lifeskill_web/credAward-faris/main.py
+++ lifeskill_web/credAward-faris/main.py
@@ -11,6 +11,7 @@ import api.IdentifyFile
 import api.IdentificationResponse
 import api.IdentificationProfile
 from dotenv import load_dotenv
+<<<<<<< HEAD
 import ffmpeg
 import pyaudio
 import wave
@@ -66,10 +67,19 @@ def record_save():
 audio_file = filename
 sub_key = '84517f5a452f4a6a8a1a570bab6f40a5'
 profile = '201f5139-1049-4dc8-9a99-b7ce0b8e2dd6'
+=======
+
+load_dotenv()
+
+audio_file = 'voices/voice4andy.wav'
+sub_key = os.environ.get("KEY1")
+profile = "7ed74eae-db79-4966-907f-c48a494994a9"
+>>>>>>> connecting front to back 50% progress
 
 # helper = api.IdentificationServiceHttpClientHelper.IdentificationServiceHttpClientHelper(
 #     sub_key)
 
+<<<<<<< HEAD
 # speaker = api.IdentifyFile.identify_file(sub_key, audio_file, True, profile)
 
 
@@ -98,22 +108,40 @@ def my_command(audio_file):
 def my_command2():
     r = sr.Recognizer()
     with sr.Microphone() as source:
+=======
+speaker = api.IdentifyFile.identify_file(sub_key, audio_file, True, profile)
+
+
+# This functions takes input from microphone and converts input into a string
+
+
+def my_command():
+    r = sr.Recognizer()
+    with sr.AudioFile(audio_file) as source:
+>>>>>>> connecting front to back 50% progress
         print('Say something...')
         r.pause_threshold = 1
         r.adjust_for_ambient_noise(source, duration=1)
         audio = r.listen(source)
+<<<<<<< HEAD
 
     # with open("microphone-results.wav", "wb") as f:
     #     f.write(audio.get_wav_data())
     #     print(f)
 
+=======
+>>>>>>> connecting front to back 50% progress
     try:
         command = r.recognize_google(audio).lower()
         print('You said: ' + command + '\n')
     # loop back to continue to listen for commands if unrecognizable speech is received
     except sr.UnknownValueError:
         print('....')
+<<<<<<< HEAD
         command = my_command2()
+=======
+        command = my_command()
+>>>>>>> connecting front to back 50% progress
     return command
 
 
@@ -126,8 +154,22 @@ def sofia_response(audio):
 
 
 def assistant(command):
+<<<<<<< HEAD
     student_list = [sn.full_name.lower() for sn in m.Student.select()]
     points = list(range(5, 501))
+=======
+    student_list = [sn.fullname.lower() for sn in m.Student.select()]
+    points = list(range(5, 51))
+
+    if 'hello' in command:
+        day_time = int(strftime('%H'))
+        if day_time < 12:
+            sofia_response('Hello Faris. Good morning')
+        elif 12 <= day_time < 18:
+            sofia_response('Hello Faris. Good afternoon')
+        else:
+            sofia_response('Hello Faris. Good evening')
+>>>>>>> connecting front to back 50% progress
 
     for s in m.Student.select():
         if s.fullname.lower() in command.lower():
@@ -167,6 +209,7 @@ def assistant(command):
                         sofia_response("cant hear you")
 
 
+<<<<<<< HEAD
 source = sr.Microphone()
 r = sr.Recognizer()
 
@@ -215,3 +258,9 @@ sleep(9999999)
 
 # else:
 #     sofia_response("you are not authorized to speak to me")
+=======
+if speaker == profile:
+    assistant(my_command())
+else:
+    sofia_response("I do not listen to you")
+>>>>>>> connecting front to back 50% progress
