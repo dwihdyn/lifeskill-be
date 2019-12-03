75c1cc55ac51a1825cdd7d2c1b9bf9d59c68590c
Faris <mfarismalik@gmail.com>
2019-12-03 18:44:00 +0800

    fix favorite, missing sending id

--- lifeskill_api/blueprints/calendar/views.py
+++ lifeskill_api/blueprints/calendar/views.py
@@ -1,21 +1,22 @@
-# for managing clubs & activities 
+# for managing clubs & activities
 
 from flask import Blueprint, jsonify, request
 from models.calendar import Club, Activity
 from models.favourite import Student_Activity, Student_Club
 import peewee as pw
 
-calendar_api_blueprint = Blueprint('calendar_api', __name__, template_folder='templates')
+calendar_api_blueprint = Blueprint(
+    'calendar_api', __name__, template_folder='templates')
 
 
 # display all clubs
 @calendar_api_blueprint.route('/clubs', methods=['GET'])
 def show_clubs():
-# get a club
-# construct a dict that represents a club
-# return all the dicts to a list
-# convert the list into a JSON
-    fav_club = Student_Club.select().where(Student_Club.student_id==1)
+    # get a club
+    # construct a dict that represents a club
+    # return all the dicts to a list
+    # convert the list into a JSON
+    fav_club = Student_Club.select().where(Student_Club.student_id == 1)
     favs = []
     for fav in fav_club:
         favs.append(fav.club_id.id)
@@ -36,10 +37,11 @@ def show_clubs():
 # add a club to favourites
 @calendar_api_blueprint.route('/clubs/favourite', methods=['POST'])
 def fave_club():
-    student_id = request.json['student_id']
+    student_id = int(request.json['student_id'])
     club_id = request.json['category_id']
 
-    query = Student_Club.select().where(Student_Club.student_id == student_id, Student_Club.club_id == club_id)
+    query = Student_Club.select().where(Student_Club.student_id ==
+                                        student_id, Student_Club.club_id == club_id)
     if query.exists():
         query[0].delete_instance()
     else:
@@ -51,7 +53,7 @@ def fave_club():
 # display all activities
 @calendar_api_blueprint.route('/activities', methods=['GET'])
 def show_activities():
-    fav_activity = Student_Activity.select().where(Student_Activity.student_id==2)
+    fav_activity = Student_Activity.select().where(Student_Activity.student_id == 2)
     favs = []
     for fav in fav_activity:
         favs.append(fav.activity_id.id)
@@ -69,19 +71,17 @@ def show_activities():
     return jsonify(activities)
 
 
-
 # add an activity to favourites
 @calendar_api_blueprint.route('/activities/favourite', methods=['POST'])
 def fave_activity():
     student_id = request.json['student_id']
     activity_id = request.json['category_id']
 
-    query = Student_Activity.select().where(Student_Activity.student_id == student_id, Student_Activity.activity_id == activity_id)
+    query = Student_Activity.select().where(Student_Activity.student_id ==
+                                            student_id, Student_Activity.activity_id == activity_id)
     if query.exists():
         query[0].delete_instance()
     else:
         Student_Activity.create(student_id=student_id, activity_id=activity_id)
 
     return jsonify(True)
-
-
