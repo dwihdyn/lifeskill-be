e216ba9669753306fff9c86ceca00d0ece034d44
Faris <mfarismalik@gmail.com>
2019-12-03 15:15:45 +0800

    tried fix

--- app.py
+++ app.py
@@ -5,13 +5,7 @@ from models.base_model import db
 import peeweedbevolve
 import models as m
 import speech_recognition as sr
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 import os
->>>>>>> connecting front to back 50% progress
-=======
->>>>>>> front end & back end connectivity established
 import sys
 import re
 import requests
@@ -21,14 +15,8 @@ import api.IdentificationServiceHttpClientHelper
 import api.IdentifyFile
 import api.IdentificationResponse
 import api.IdentificationProfile
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
 from lifeskill_api.blueprints.audio.views import takeAudio
->>>>>>> connecting front to back 50% progress
-=======
->>>>>>> front end & back end connectivity established
 
 web_dir = os.path.join(os.path.dirname(
     os.path.abspath(__file__)), 'instagram_web')
@@ -52,12 +40,3 @@ def _db_close(exc):
         print(db)
         print(db.close())
     return exc
-<<<<<<< HEAD
-
-
-@app.cli.command("assistant")
-def x():
-    from lifeskill_api.blueprints.students.credAward_faris.main import assistant, my_command2
-    assistant(my_command2)
-=======
->>>>>>> connecting front to back 50% progress
